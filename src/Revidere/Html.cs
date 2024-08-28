b0950fdea2b693af50163ce072702e12b30cfd62
AASeq <aaseq@aaseq.com>
2024-08-27 19:40:26 -0700

    Added nohistory style

--- src/Revidere/Html.cs
+++ src/Revidere/Html.cs
@@ -42,16 +42,19 @@ internal static class Html {
                 sb.AppendLine($"""            <div class="title">{checkState.Check.Properties.Title}</div>""");
                 sb.AppendLine("""            <div class="semaphore"></div>""");
 
-                sb.AppendLine("""            <div class="history">""");
                 if (showHistory && (check is not CompositeCheck)) {
+                    sb.AppendLine("""            <div class="history">""");
                     var i = 0;
                     foreach (var state in checkState.HealthHistory) {
                         sb.AppendLine("                " + (state ? """<div class="historicalOk"></div>""" : """<div class="historicalNok"></div>"""));
                         i++;
                         if (i >= 10) { break; }
                     }
+                    sb.AppendLine("""            </div>""");
+                } else {
+                    sb.AppendLine("""            <div class="nohistory">""");
+                    sb.AppendLine("""            </div>""");
                 }
-                sb.AppendLine("""            </div>""");
 
                 sb.AppendLine("""        </div>""");
 
